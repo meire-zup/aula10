e92cb552304e7aee8260857fc7e897de0cce667b
meire-zup <jucemeire.lopes@zup.com.br>
2023-05-28 18:36:15 -0300

    correção exercicio 2 - estrutura de decisa

--- src/estrutura_de_decisao/Exercicio2.java
+++ src/estrutura_de_decisao/Exercicio2.java
@@ -21,8 +21,14 @@ public class Exercicio2 {
             System.out.println("O produto mais indicado a ser comprado é o produto 2. Boas compras!");
         } else if (preco3 < preco1 && preco3 < preco2) {
             System.out.println("O produto mais indicado a ser comprado é o produto 3. Boas compras!");
-        } else if (preco1 == preco2 && preco1 == 3 && preco2 == preco3) {
+        } else if (preco1 == preco2 && preco1 == preco3 && preco2 == preco3) {
             System.out.println("O preço dos produtos são iguais! ");
+        } else if(preco1 == preco2 & preco1<preco3){
+            System.out.println("Produto 1 e 2 são iguais e mais baratos que o produto 3. Boas compras!");
+        } else if (preco1 == preco3 && preco1 < preco2) {
+            System.out.println("Produto 1 e 3 são iguais e mais baratos que o produto 2. Boas compras!");
+        } else   {
+            System.out.println("Produto 2 e 3 são iguais e mais baratos que o produto 1. Boas compras!");
         }
         System.out.println("---------------------------------------");
 
